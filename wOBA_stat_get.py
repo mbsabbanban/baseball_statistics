a977d3087d54bc2247e9f8d66bae6f107f3e696a
mbsabbanban <mbsabbanban@gmail.com>
2014-04-29 07:27:25 -0400

    Set up the GET request, started on the dynamic URL

--- wOBA_stat_get.py
+++ wOBA_stat_get.py
@@ -1,1 +1,15 @@
 #!/bin/python/
+
+import urllib.request
+
+#Setting up a dynamic url
+
+baseball_url = 'http://dailybaseballdata.com/cgi-bin/getstats.pl?date=426&out=csv'
+
+#Setting up the GET request to retrieve the HTML markup
+
+req = urllib.request.Request('baseball_url')
+response = urllib.request.urlopen(req)
+html = response.read()
+
+#Now I need to find some way to parse the markup and write to a text file
