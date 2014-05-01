00523824a3944027e39678fc11967d2b79a20abc
mbsabbanban <mbsabbanban@gmail.com>
2014-05-01 08:30:15 -0400

    Created new function getBaseballData() that encapsulates objective 1

--- wOBA_stat_get.py
+++ wOBA_stat_get.py
@@ -6,42 +6,52 @@ from datetime import date, timedelta
 from bs4 import BeautifulSoup
 import re
 
-#Global Variables
+
+##### DECLARING GLOBAL VARIABLES #####
 yesterday = date.today() - timedelta(1)
 
 #Function for getting raw daily baseball data
+def getBaseballData():
 
-#Setting up a dynamic url
+	#Setting up a dynamic url
 
-baseball_url = 'http://dailybaseballdata.com/cgi-bin/getstats.pl?date=' + yesterday.strftime("%m%d") + '&out=csv'
+	baseball_url = 'http://dailybaseballdata.com/cgi-bin/getstats.pl?date=' + yesterday.strftime("%m%d") + '&out=csv'
 
-print(baseball_url)
+	print(baseball_url)
 
-#Setting up the GET request to retrieve the HTML markup
+	#Setting up the GET request to retrieve the HTML markup
 
-req = urllib.request.Request(baseball_url)
-response = urllib.request.urlopen(req)
-html = response.read()
+	req = urllib.request.Request(baseball_url)
+	response = urllib.request.urlopen(req)
+	html = response.read()
 
-#Using BeautifulSoup to parse the markup for info, removing script and other unnecessary tags
+	#Using BeautifulSoup to parse the markup for info, removing script and other unnecessary tags
 
-clean_html = BeautifulSoup(html)
-to_extract = clean_html.findAll('script')
+	clean_html = BeautifulSoup(html)
+	to_extract = clean_html.findAll('script')
 
-for item in to_extract:
-	item.extract()
+	for item in to_extract:
+		item.extract()
 
-clean_html = clean_html.get_text()
-clean_html = clean_html.strip() #strip removes all the whitespace
-clean_html = '\n'.join(clean_html.split('\n')[5:]) #Slice at the end removes the first 5 lines and then joining together by '\n'
+	clean_html = clean_html.get_text()
+	clean_html = clean_html.strip() #strip removes all the whitespace
+	clean_html = '\n'.join(clean_html.split('\n')[5:]) #Slice at the end removes the first 5 lines and then joining together by '\n'
 
-print(clean_html)
+	print(clean_html)
 
-#I am creating a test file and then writing to it using open(),write(),close()
+	#I am creating a test file and then writing to it using open(),write(),close()
 
-f = open("test.txt", "w")
-f.write(clean_html)
-f.close()
+	f = open("test.txt", "w")
+	f.write(clean_html)
+	f.close()
+
+##### END OF getBaseballData FUNCTION #####
+
+
 
 #DATABASE insert should probably be declared in a new function
 #http://stackoverflow.com/questions/10154633/load-csv-data-into-mysql-in-python)
+
+##### RUNNING THE FUNCTIONS TO GENERATE REPORT $$$$$
+
+getBaseballData()
