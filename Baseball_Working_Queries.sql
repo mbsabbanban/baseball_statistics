d9e68feb593fa503597b22862daacef72fc2812f
Peter Ibarra <peteribarra@Peters-MacBook-Pro.local>
2015-03-24 17:34:28 -0400

    Even More Updates for OBP

--- Baseball_Working_Queries.sql
+++ Baseball_Working_Queries.sql
@@ -6,6 +6,11 @@
 select ((SUM(H)+SUM(BB)+SUM(HBP)))/(SUM(AB)+SUM(SF)+SUM(BB)+SUM(HBP)) as AngelsOBP from Batting where teamID = 'LAA' and yearID = 2014;
 
 
+#On Base Percentage Query - ALL TEAMS 2004+ 
+
+select ((SUM(H)+SUM(BB)+SUM(HBP)))/(SUM(AB)+SUM(SF)+SUM(BB)+SUM(HBP)) as TeamOBP, teamID from Batting where yearID >= 2004 GROUP BY teamID ORDER BY TeamOBP DESC;
+
+
 #Battery AVG Query - ANGELS 2014
 
 select (SUM(H)/SUM(AB)) as AngelsAVG from Batting where teamID = 'LAA' and yearID = 2014;
