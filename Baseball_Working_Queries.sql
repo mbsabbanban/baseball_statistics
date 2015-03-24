6b42ab321dd314b5e0755b6665f9d2fc4e4a723e
Peter Ibarra <peteribarra@Peters-MacBook-Pro.local>
2015-03-24 17:40:20 -0400

    Websites Added

--- Baseball_Working_Queries.sql
+++ Baseball_Working_Queries.sql
@@ -14,3 +14,9 @@ select ((SUM(H)+SUM(BB)+SUM(HBP)))/(SUM(AB)+SUM(SF)+SUM(BB)+SUM(HBP)) as TeamOBP
 #Battery AVG Query - ANGELS 2014
 
 select (SUM(H)/SUM(AB)) as AngelsAVG from Batting where teamID = 'LAA' and yearID = 2014; 
+
+
+#RANDOM WEBSITES
+#http://www.baseball-reference.com/about/war_explained.shtml
+#http://www.fangraphs.com/library/war/war-position-players/
+#http://battingleadoff.com/2013/12/31/comparing-the-3-war-measures-2/
