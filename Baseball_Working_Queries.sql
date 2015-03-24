c5dd75de8727a74328a232203d2b32c676eb51ca
Peter Ibarra <peteribarra@Peters-MacBook-Pro.local>
2015-03-24 17:08:33 -0400

    Added Query for Angels Team OBP in 2014

--- Baseball_Working_Queries.sql
+++ Baseball_Working_Queries.sql
@@ -1,3 +1,8 @@
 #Baseball Working Queries 
 
 select * from Teams; 
+
+
+#On Base Percentage Query - ANGELS 2015
+
+select AVG((H+BB+IBB)/AB) as AngelsOBP from Batting where teamID = 'LAA' and yearID = 2014 AND AB >=10;
